d3d8f463122e631ae4b8a14c3bba8ea51ad45cb5
htmltest <struev.vs@gmail.com>
2020-06-30 16:21:13 +0300

    first html

---
+++ js/tools.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false
+    });
+
+});
